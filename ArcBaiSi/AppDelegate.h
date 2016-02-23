ec29a706f318897d2fe114ef6d56452b440a72a1
Arc-lin <m13643046965@163.com>
2016-02-23 23:12:06 +0800

    Initial Commit

---
+++ ArcBaiSi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArcBaiSi
+//
+//  Created by Arclin on 16/2/23.
+//  Copyright © 2016年 sziit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArcBaiSi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArcBaiSi
+//
+//  Created by Arclin on 16/2/23.
+//  Copyright © 2016年 sziit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
